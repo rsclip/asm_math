4b44bca754c822fb519795abcdef6ec0cb2f232f
cy <40525455+rsclip@users.noreply.github.com>
2024-03-22 00:50:40 +0000

    u128 sub

--- src/u128.rs
+++ src/u128.rs
@@ -40,6 +40,34 @@ impl U128 {
 
         (result_high, result_low)
     }
+
+    /// Subtract two 128 bit integers
+    /// Returns a tuple of high and low 64 bit integers
+    fn subc(&self, other: &Self) -> (u64, u64) {
+        let mut result_high = 0u64;
+        let mut result_low = 0u64;
+        let mut overflow_flag: i8 = 0;
+
+        unsafe {
+            asm!(
+                "sub {low1}, {low2}",
+                "sbb {high1}, {high2}",
+                "setc {ovf}",
+                low1 = inout(reg) self.low => result_low,
+                high1 = inout(reg) self.high => result_high,
+                low2 = in(reg) other.low,
+                high2 = in(reg) other.high,
+                ovf = out(reg_byte) overflow_flag,
+                options(nostack),
+            );
+        }
+
+        if overflow_flag == 1 {
+            panic!("U128 subtraction overflow")
+        }
+
+        (result_high, result_low)
+    }
 }
 
 mod op_impl {
@@ -53,6 +81,15 @@ mod op_impl {
             Self { high, low }
         }
     }
+
+    impl std::ops::Sub for U128 {
+        type Output = Self;
+
+        fn sub(self, other: Self) -> Self {
+            let (high, low) = self.subc(&other);
+            Self { high, low }
+        }
+    }
 }
 
 mod misc_impl {
@@ -61,6 +98,13 @@ mod misc_impl {
     impl std::fmt::Debug for U128 {
         fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
             write!(f, "U128({:b} {:b})", self.high, self.low)
+        }
+    }
+
+    impl std::fmt::Display for U128 {
+        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+            let v: u128 = (self.high as u128) << 64 | self.low as u128;
+            write!(f, "{}", v)
         }
     }
 
@@ -135,7 +179,7 @@ mod test {
         let a = U128::from(10u64);
         let b = U128::from(20u8);
         let c = a + b;
-        println!("{:?} + {:?} = {:?}", a, b, c);
+        println!("{a:?} ({a}) + {b:?} ({b}) = {c:?} ({c})");
         assert_eq!(c.low, 30);
         assert_eq!(c.high, 0);
     }
@@ -145,7 +189,7 @@ mod test {
         let a = U128::from(u64::MAX);
         let b = U128::from(1usize);
         let c = a + b;
-        println!("{:?} + {:?} = {:?}", a, b, c);
+        println!("{a:?} ({a}) + {b:?} ({b}) = {c:?} ({c})");
         assert_eq!(c.low, 0);
         assert_eq!(c.high, 1);
     }
@@ -155,6 +199,27 @@ mod test {
     fn overflow_unsafe() {
         let a = U128::from(u128::MAX);
         let d = a + a + a + a;
-        println!("{a:?} * 4 = {d:?}");
+        println!("{a:?} ({a}) * 4 = {d:?} ({d})");
+    }
+
+    #[test]
+    fn sub() {
+        let a = U128::from(20u64);
+        let b = U128::from(10u8);
+        let c = a - b;
+        println!("{a:?} ({a}) - {b:?} ({b}) = {c:?} ({c})");
+        assert_eq!(c.low, 10);
+        assert_eq!(c.high, 0);
+    }
+
+    #[test]
+    #[should_panic]
+    fn sub_overflow() {
+        let a = U128::from(10u64);
+        let b = U128::from(20u8);
+        let c = a - b;
+        println!("{a:?} ({a}) - {b:?} ({b}) = {c:?} ({c})");
+        assert_eq!(c.low, u64::MAX);
+        assert_eq!(c.high, u64::MAX);
     }
 }
